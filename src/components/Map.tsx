18269bc3b8e8e68e76b0fb1a54c647a100dde423
Janne Peippo <peippo@gmail.com>
2023-01-08 10:58:25 +0200

    Adjust map tooltip

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { trpc } from "@utils/trpc";
+import { useRouter } from "next/router";
 import DeckGL from "@deck.gl/react/typed";
 import { IconLayer } from "@deck.gl/layers/typed";
 import maplibregl from "maplibre-gl";
@@ -10,6 +11,7 @@ import "maplibre-gl/dist/maplibre-gl.css";
 import { mapStyle } from "@styles/map-style";
 
 import BikeIcon from "./icons/BikeIcon";
+import { kebabCase } from "@utils/general";
 
 const INITIAL_VIEW_STATE = {
   longitude: 24.9235379,
@@ -22,12 +24,14 @@ const INITIAL_VIEW_STATE = {
 const Map = () => {
   const stations = trpc.station.getAll.useQuery();
   const [hoverInfo, setHoverInfo] = useState<PickingInfo>();
+  const router = useRouter();
 
   const data = stations.data?.map((s) => {
     return {
       coordinates: [s.longitude, s.latitude],
       capacity: s.capacity,
       name: s.name,
+      stationId: s.stationId,
     };
   });
 
@@ -39,10 +43,16 @@ const Map = () => {
     iconMapping: "/marker-atlas-mapping.json",
     sizeScale: 1,
     sizeMinPixels: 20,
+    autoHighlight: true,
     getPosition: (d) => d.coordinates,
     getIcon: () => "station",
     getSize: (d) => d.capacity,
     onHover: (info) => setHoverInfo(info as PickingInfo),
+    onClick: (info) => {
+      router.push(
+        `/stations/${info.object.stationId}-${kebabCase(info.object.name)}`
+      );
+    },
   });
 
   return (
@@ -51,7 +61,7 @@ const Map = () => {
         initialViewState={INITIAL_VIEW_STATE}
         controller={true}
         layers={[stationsIconLayer]}
-        getCursor={({ isHovering }) => (isHovering ? "crosshair" : "grab")}
+        getCursor={({ isHovering }) => (isHovering ? "pointer" : "grab")}
       >
         <ReactMapGl
           // @ts-ignore
@@ -61,7 +71,7 @@ const Map = () => {
         />
         {hoverInfo?.object && (
           <div
-            className="z-1 pointer-events-none absolute -translate-x-1/2 -translate-y-full rounded-lg bg-sky-800 py-2 px-3 text-center text-slate-300 shadow-lg"
+            className="z-1 pointer-events-none absolute -translate-x-1/2 -translate-y-[calc(100%+10px)] rounded-lg border-b-2 border-slate-900 bg-sky-800 px-3 py-2 text-center text-slate-300 shadow-lg"
             style={{
               left: hoverInfo.x,
               top: hoverInfo.y,
@@ -74,6 +84,7 @@ const Map = () => {
               <strong>{hoverInfo.object.capacity}</strong>
               <BikeIcon width={24} className="ml-2" />{" "}
             </p>
+            <div className="absolute left-1/2 bottom-0 z-0 h-3 w-3 -translate-x-1/2 translate-y-1/2 rotate-45 transform border-r border-b border-slate-900 bg-sky-800"></div>
           </div>
         )}
       </DeckGL>
