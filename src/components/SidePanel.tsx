e569ba31d735dfc77565857fc33fc24bc23a9626
Janne Peippo <peippo@gmail.com>
2023-01-08 18:25:10 +0200

    Add single station page

--- src/components/SidePanel.tsx
+++ src/components/SidePanel.tsx
@@ -1,12 +1,19 @@
+import classNames from "classnames";
 import React from "react";
 
 type Props = {
   children: React.ReactNode;
+  width?: "narrow" | "wide";
 };
 
-const SidePanel: React.FC<Props> = ({ children }) => {
+const SidePanel: React.FC<Props> = ({ children, width = "narrow" }) => {
   return (
-    <div className="absolute top-10 right-10 h-[calc(100vh-5rem)] w-64 overflow-y-scroll rounded-lg border border-cyan-800 p-5 backdrop-blur-lg">
+    <div
+      className={classNames(
+        "absolute top-10 right-10 z-10 max-h-[calc(100vh-5rem)] overflow-x-hidden overflow-y-scroll rounded-lg border border-cyan-800 bg-slate-900 bg-opacity-70 p-5 drop-shadow-2xl backdrop-blur-lg",
+        width === "narrow" ? "w-64" : "w-96"
+      )}
+    >
       {children}
     </div>
   );
---
+++ src/pages/stations/[stationId].tsx
@@ -0,0 +1,75 @@
+import { type NextPage } from "next";
+import { useRouter } from "next/router";
+import Head from "next/head";
+import { trpc } from "@utils/trpc";
+
+import SidePanel from "@components/SidePanel";
+import {
+  ArrivalsIcon,
+  BackIcon,
+  BikeIcon,
+  DeparturesIcon,
+} from "@components/icons/Icons";
+import Link from "next/link";
+
+const Station: NextPage = () => {
+  const router = useRouter();
+  const { stationId } = router.query;
+
+  const station = trpc.station.getSingle.useQuery(
+    {
+      stationId: parseInt(stationId as string),
+    },
+    { enabled: !!stationId }
+  );
+
+  return (
+    <>
+      <Head>
+        <title>
+          {station.data?.name} [{station.data?.stationId}]
+        </title>
+      </Head>
+      <SidePanel width="wide">
+        <Link className="group mb-3 inline-flex items-center" href="/">
+          <BackIcon width={12} className="mr-2 text-yellow-500" />
+          <span className="border-b border-slate-600 group-hover:text-yellow-500">
+            All stations
+          </span>
+        </Link>
+        {station.data && (
+          <div className="flex flex-col">
+            <div className="mb-4 flex items-center justify-between">
+              <div>
+                <h2 className="text-xl">{station.data.name}</h2>
+                <span className="text-sm text-slate-400">
+                  {station.data.address}
+                </span>
+              </div>
+              <div className="align-center flex aspect-square items-center justify-center rounded-full border-l bg-yellow-400 px-3 text-slate-900">
+                <span className="sr-only">Station ID: </span>
+                <span>{station.data.stationId}</span>
+              </div>
+            </div>
+            <div className="mb-4 flex items-center">
+              <BikeIcon width={24} className="mr-2 text-yellow-500" />
+              <p>Capacity {station.data.capacity} bikes</p>
+            </div>
+            <div className="flex items-center justify-center border-t border-b border-cyan-800">
+              <div className="flex flex-grow justify-center px-2 py-3">
+                <ArrivalsIcon width={22} className="mr-3 text-yellow-500" />
+                <p>{station.data.arrivals.length} arrivals</p>
+              </div>
+              <div className="flex flex-grow items-center justify-center border-l border-cyan-800 px-2 py-3">
+                <DeparturesIcon width={22} className="mr-3 text-yellow-500" />
+                <p>{station.data.departures.length} departures</p>
+              </div>
+            </div>
+          </div>
+        )}
+      </SidePanel>
+    </>
+  );
+};
+
+export default Station;
