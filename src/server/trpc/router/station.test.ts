338754d085a659778362f9629ea59e9b443ef3a2
Janne Peippo <peippo@gmail.com>
2023-01-09 19:54:30 +0200

    Add tests for station router

---
+++ src/server/trpc/router/station.test.ts
@@ -0,0 +1,34 @@
+import { describe, expect, test } from "vitest";
+import { createContextInner } from "../context";
+import { AppRouter, appRouter } from "./_app";
+import type { inferProcedureInput } from "@trpc/server";
+
+describe("Station router", () => {
+  test("should return list of stations", async () => {
+    const ctx = await createContextInner({});
+    const caller = appRouter.createCaller(ctx);
+
+    const stations = await caller.station.getAll();
+
+    expect(stations).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          name: "Kaivopuisto",
+        }),
+      ])
+    );
+  });
+
+  test("should return single station", async () => {
+    const ctx = await createContextInner({});
+    const caller = appRouter.createCaller(ctx);
+
+    const input: inferProcedureInput<AppRouter["station"]["getSingle"]> = {
+      stationId: 1,
+    };
+
+    const station = await caller.station.getSingle(input);
+
+    expect(station?.name).toBe("Kaivopuisto");
+  });
+});
