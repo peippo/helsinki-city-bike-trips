669d077f82b1176a7fa7a0d93227c97da4f366bf
Janne Peippo <peippo@gmail.com>
2023-01-11 20:43:00 +0200

    Adjust side panel size

--- src/components/SidePanel.tsx
+++ src/components/SidePanel.tsx
@@ -10,8 +10,8 @@ const SidePanel: React.FC<Props> = ({ children, width = "narrow" }) => {
   return (
     <div
       className={classNames(
-        "sidepanel absolute top-10 right-10 z-10 max-h-[calc(100vh-5rem)] overflow-x-hidden overflow-y-scroll rounded-lg border border-cyan-800 bg-slate-900 bg-opacity-70 p-5 drop-shadow-2xl backdrop-blur-lg",
-        width === "narrow" ? "w-64" : "w-96"
+        "sidepanel absolute top-5 right-5 z-10 overflow-x-hidden overflow-y-scroll rounded-lg border border-cyan-800 bg-slate-900 bg-opacity-70 p-5 drop-shadow-2xl backdrop-blur-lg",
+        width === "narrow" ? "max-h-[50vh] w-64" : "w-96"
       )}
     >
       {children}
